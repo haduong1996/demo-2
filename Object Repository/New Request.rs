8e05044fd873af9ebbf656afd99f8a0512376329
Duong Ha <Duong Ha@DESKTOP-9NDSVDC>
2018-05-12 14:04:45 +0700

    lại demo

---
+++ Object Repository/New Request.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>New Request</name>
+   <tag></tag>
+   <elementGuidId>8f8e990e-2a9d-4c72-86a2-ebd5c622f4de</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer WHJ6qM-kB7s8GSYPvbzR69GdE9FUnW3oNhlGHdE_9pYdZclZtc1mBviITO-6FWsOtV8gzMZh5bBSMy25cbqHCX-wfyyGKWEiuv192Fo1dbzoBemGREyE1mtl3lr8jaRI4EDR60DHMY8M7s-pNKZ9mS7AZN_8WL8d1BVVXnRV2BpERwJEeLpvlAN_DNu27ytpkS3i2VJhsNdICzgPRrDcL0ul-PS43iOplXQk_kQBvkSMYmZIawTB_medxbHVMJyoil7fDa12asge18JMdIArRm9xGqyKDP2HxEQOQQyrywMhF4vSiej1OrtR-9BiBnAVhnE_P9qvWblwZbzRjOrXHZ7VWGxHAxlCKlISZr2vxsfdOroCPVe7F7dPYEeyihML733Iw_TXNJfRCBDQchtfj3VCeN98sn3P8XzERop_9KjXZ8SGlguLmJWIfyz4Dz0j5CDoM7nBfQeGttFVg1tmS4gW_TMbw5J5eeFb276Q1YEPD2kmrlnXW7HuPg9pTPLQM2Lc9RWgWIxcLIv_ofK5yEXb8eBX4NbOfjnjGenoTGcfXYlRYkmIyfwRNIn4k9LX2kkpfmqbKD4Z5yV_Jqq4TxmnFKJUI1HnPHbx7O_mFdTg4rULtao4basAgfwBEjfTkoYiR3-neXmG8Koi8Alu_c-WZ6NhSLsy_CV9lXqyU4K2wFt241HUG2DOeQZNrW8mjiOHoznlmYn0O-IAz9fqHkhtWhOZQMU-IEwQIt8EqQm-2UugFjUp69Q4JKxCRc0pex2ACe4Dxao2OeAjo67qZH9KkbdguT6A8D147cHO-YMo7GIdJd9MyF4HJWcIoK0pQOsJjPCgp-3swFm7gHyZON7eo7pH1-023gwkp6W-t7Gz8vZljM-J6US1PMPpjgkKak0tlOE7kNO3CSQGIFz-bfBbtJdzq3Imr7qoaOmmb0sDmdFMfvqwRbgmdXhLV55EF2zBPvNxqpE3Jj4UsqCGUGKn81LZKuExM6zxSmrvYXpOdASYL8g5NYdBowbKbfl7EuTVWp9fIOuXnbiKOkkVCOuB5CwrlfBZiaTBD8_-6vvOPYxbzEiN8s7k7fIu0L34RX-IKTxehFiYt8DMngUtyBuZwsHjHkThxpg0aqdxBA4JfD75d-nLzHlRB-mq0AZblbU2irM4Bxeh0QatxZiMRoB32cLHZzSpJIk5cOKg4Wq_VH4QIMyKgwmrQqxs9-CN1QmwiRRDvdJVnasWSvwE2WUGkLjZJR_Kdq7FXB0P6ez4aRHyhdz6w86OBf4HNf4_G2Icfcz1DyqeWaXTFCQXwl3EqN4wzBZRXRYxz9iQ_bByY6wW3imSizPY5PhNMIkahskfZsJcuLRiTzCf8Ao6TWnEkdKyCL4JKPwvatoxh9TNfNbClig7QvTArOCuC9hYolw5escbvhhRpgi7F5m85oofwUXZvlmchuB0HFchKc3IdCbRCZbSNlSYNGarSqeZcqYNpfqVj1y5BC2UijCrVrCk2xnhhMzBrno4S8UQnp1YfxpwzB0zUxJtRuoSWiPbnDW6o17_LJkec4Js4Y3HHh306--scVMGG9IEJ25p3rF19AhFEdz6py4Mvufx83_PWd-UnBZfQhVfsHzw97xdCnoY6UNKpAqGnU-UDgOPJkysQFZv-6JJyR9xelxYApUnLMyNNcisFt-OWLtrlgG-ms4m1V7gQcA8kwbhysm16F3LlPFJ6GtzM4Mf0lnQyZ7101atFlTDjv24jBWwaYSEO1p4HYTVlwbi54zRDy88fSqvNv8UK9qGrZHFKTQeXzuiCBTEOEOPhFZkskZBS_yBiO8Jtq523p8kcoc1qsuW-lir9eKhZLVbcZVHMrvvDsKNLyXOybbOJjbaL535uFT2mnw3bjqm0pJcrnwzyOIeUC08IQ9NPuOA0YvOyIAMBvddGkNvjp7GqHIWhxVfGej-Sw9hu-3xOEzoLHXYmwnkPxUd5omvZLag8VTWudX8dX9qscc9KcL4PnM66Z5tWHrE-I_SJTh37HTkBCPaQZhSx9k</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>http://vinsoft.ezcloudhotel.com/api/NewGroupMaster/GetTAMasterSearch?accountInfo=&amp;accountName=&amp;country=&amp;class=&amp;isAP=&amp;isAR=&amp;isCO=&amp;salesPerson=</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
